ff1acac2ff9803373a107c68e9cb1cba2dbdc98e
amogh429 <amogh.allrounder@gmail.com>
2026-07-17 19:20:24 +0530

    feat: Set up Express server and middleware

---
+++ Backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Student Course Management System API is running successfully!",
+  });
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
