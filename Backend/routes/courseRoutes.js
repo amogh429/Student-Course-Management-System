24b15a2e4898349a6f96bf53f430d2a1b4e38fe7
amogh429 <amogh.allrounder@gmail.com>
2026-07-19 16:20:06 +0530

    feat: Implement update course controller

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -3,11 +3,13 @@ import {
   createCourse,
   getAllCourses,
   getCourseById,
+  updateCourse,
 } from "../controllers/courseController.js";
 
 const router = express.Router();
 router.post("/", createCourse);
 router.get("/", getAllCourses);
 router.get("/:id", getCourseById);
+router.put("/:id", updateCourse);
 
 export default router;
