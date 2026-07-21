709ddc5ef2c08429c6b18f41529df973ef08bbee
amogh429 <amogh.allrounder@gmail.com>
2026-07-21 12:46:46 +0530

    feat: Implement create student controller

--- Backend/controllers/studentController.js
+++ Backend/controllers/studentController.js
@@ -1,20 +1,34 @@
+import Student from "../models/Student.js";
 
-export const createStudent = (req, res) => {
-    res.send("Create Student API");
+export const createStudent = async (req, res) => {
+  try {
+    const student = await Student.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "Student created successfully",
+      data: student,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const getAllStudents = (req, res) => {
-    res.send("Get All Students API");
+  res.send("Get All Students API");
 };
 
 export const getStudentById = (req, res) => {
-    res.send("Get Student By ID API");
+  res.send("Get Student By ID API");
 };
 
 export const updateStudent = (req, res) => {
-    res.send("Update Student API");
+  res.send("Update Student API");
 };
 
 export const deleteStudent = (req, res) => {
-    res.send("Delete Student API");
+  res.send("Delete Student API");
 };
