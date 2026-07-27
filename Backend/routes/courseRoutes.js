f4939095ce76a712ea3e4f5dfe555ce0b482fa90
amogh429 <amogh.allrounder@gmail.com>
2026-07-27 21:12:55 +0530

    feat: Add JWT authentication middleware

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -6,12 +6,13 @@ import {
   updateCourse,
   deleteCourse,
 } from "../controllers/courseController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.post("/", createCourse);
+router.post("/",protect, createCourse);
 router.get("/", getAllCourses);
 router.get("/:id", getCourseById);
-router.put("/:id", updateCourse);
-router.delete("/:id",deleteCourse);
+router.put("/:id",protect, updateCourse);
+router.delete("/:id",protect,deleteCourse);
 
 export default router;
