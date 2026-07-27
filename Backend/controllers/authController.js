e0bd878605cf3227c73748f1de8a16aa22f2d53f
amogh429 <amogh.allrounder@gmail.com>
2026-07-27 21:08:26 +0530

    feat: Implement user login API with JWT authentication

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import generateToken from "../utils/generateToken.js";
 
 export const registerUser = async (req, res) => {
     try {
@@ -27,6 +28,54 @@ export const registerUser = async (req, res) => {
         res.status(201).json({
             success: true,
             message: "User registered successfully",
+            data: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = generateToken(user._id);
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            token,
             data: {
                 id: user._id,
                 name: user.name,
