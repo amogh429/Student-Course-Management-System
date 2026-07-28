3b22ab8bd0489faa095dca507aaf4b8ecb269a1b
amogh429 <amogh.allrounder@gmail.com>
2026-07-28 19:25:37 +0530

    feat: Fetch and display students data

--- Frontend/src/pages/Students.jsx
+++ Frontend/src/pages/Students.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Students() {
-  return <h2>Students Page</h2>;
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const res = await api.get("/students");
+        setStudents(res.data.data);
+      } catch (error) {
+        console.error(error);
+        alert("Failed to load students");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStudents();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading students...</h2>;
+  }
+
+  return (
+    <div>
+      <h2>Students</h2>
+
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Student ID</th>
+            <th>Email</th>
+            <th>Department</th>
+            <th>Semester</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {students.map((student) => (
+            <tr key={student._id}>
+              <td>{student.studentName}</td>
+              <td>{student.studentId}</td>
+              <td>{student.email}</td>
+              <td>{student.department}</td>
+              <td>{student.semester}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Students;
