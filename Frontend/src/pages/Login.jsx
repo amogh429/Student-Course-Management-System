5c812f916cf7b7b31313e7099ccd3cacbe2be5ec
amogh429 <amogh.allrounder@gmail.com>
2026-07-28 12:49:21 +0530

    feat: Create Axios service and connect register page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,5 +1,69 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
+
 function Login() {
-  return <h2>Login Page</h2>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/auth/login", formData);
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful");
+
+      navigate("/");
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
