76def839d8110d2b3fcc25033b30fa7e32f0cd57
amogh429 <amogh.allrounder@gmail.com>
2026-07-30 12:48:49 +0530

    feat: Code cleanup

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -8,12 +8,10 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
 
-    console.log("TOKEN:",token);
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    console.log("Headers:",config.headers);
     return config;
   },
   (error) => Promise.reject(error),
